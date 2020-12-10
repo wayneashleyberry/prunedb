525a5f489d7bd0a28b7e91a6713c311130ddf477
Wayne Ashley Berry <wayneashleyberry@gmail.com>
2020-12-10 18:56:21 +0000

    init

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/wayneashleyberry/prunedb
+
+go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/lib/pq v1.2.0 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0 // indirect
+	github.com/spf13/cobra v1.1.1
+)
---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	ctx := context.Background()
+
+	cmd := Command(ctx)
+
+	cmd.SilenceUsage = true
+
+	if err := cmd.ExecuteContext(ctx); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+type TestDatabase struct {
+	Host     string `envconfig:"TESTDB_HOST" default:"127.0.0.1"`
+	Port     string `envconfig:"TESTDB_PORT" default:"3306"`
+	User     string `envconfig:"TESTDB_USER" required:"true"`
+	Password string `envconfig:"TESTDB_PASSWORD" required:"true"`
+}
+
+// Config creates a new mysql.Config from a TestDatabase
+func (s TestDatabase) Config() *mysql.Config {
+	conf := mysql.NewConfig()
+
+	conf.Addr = s.Host + ":" + s.Port
+	conf.Net = "tcp"
+	conf.User = s.User
+	conf.Passwd = s.Password
+	conf.ParseTime = true
+	conf.Params = map[string]string{
+		"charset":         "utf8mb4",
+		"multiStatements": "true",
+	}
+
+	return conf
+}
+
+// Command will create a new "prunedb" cobra command.
+func Command(ctx context.Context) *cobra.Command {
+	var commit bool
+
+	cmd := &cobra.Command{
+		Use:   "prunedb",
+		Short: "Drop lingering test databases",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var s TestDatabase
+
+			envconfig.MustProcess("", &s)
+
+			conf := s.Config()
+
+			db, err := sqlx.ConnectContext(ctx, "mysql", conf.FormatDSN())
+			if err != nil {
+				return fmt.Errorf("connect to database: %w", err)
+			}
+
+			rows, err := db.QueryxContext(ctx, "show databases")
+			if err != nil {
+				return fmt.Errorf("execute database query: %w", err)
+			}
+
+			defer func() {
+				_ = rows.Close()
+			}()
+
+			databases := []string{}
+
+			for rows.Next() {
+				var database string
+
+				err := rows.Scan(&database)
+				if err != nil {
+					return fmt.Errorf("scan row: %w", err)
+				}
+
+				if strings.HasPrefix(database, "test_") {
+					databases = append(databases, database)
+				}
+			}
+
+			if err := rows.Err(); err != nil {
+				return fmt.Errorf("rows error: %w", err)
+			}
+
+			if len(databases) == 0 {
+				fmt.Println("no databases to prune")
+
+				return nil
+			}
+
+			for _, database := range databases {
+				query := fmt.Sprintf("drop database %s", database)
+
+				fmt.Println(query)
+
+				if !commit {
+					continue
+				}
+
+				_, err := db.ExecContext(ctx, query)
+				if err != nil {
+					return fmt.Errorf("execute database query: %w", err)
+				}
+			}
+
+			if !commit {
+				fmt.Println("use the --commit flag to run these queries")
+			}
+
+			return nil
+		},
+	}
+
+	cmd.PersistentFlags().BoolVar(&commit, "commit", false, "commit query")
+
+	return cmd
+}
